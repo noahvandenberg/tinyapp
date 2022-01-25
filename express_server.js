9bb259e0b2cf34dd7d41cbb5db74d852d7fdb0cb
Noah Vandenberg <noah@noahvandenberg.com>
2022-01-25 09:36:13 -0800

    create delete button

--- express_server.js
+++ express_server.js
@@ -16,19 +16,18 @@ const urlDatabase = {
 
 app.set('view engine', 'ejs');
 
+
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
 
+// GET Homepage
 app.get("/urls", (req, res) => {
   const templateVars = {urls: urlDatabase}
   res.render('urls_index',templateVars)
 });
 
-app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
-});
-
+// GET Specific URL Page
 app.get("/urls/:shortURL", (req, res) => {
   const templateVars = { 
     shortURL: req.params.shortURL, 
@@ -37,6 +36,19 @@ app.get("/urls/:shortURL", (req, res) => {
   res.render('urls_show', templateVars)
 });
 
+// POST delete a url
+app.post("/urls/:shortURL/delete", (req, res) => {
+  console.log(req.params.shortURL)
+  delete urlDatabase[req.params.shortURL]
+  res.redirect("/urls")
+})
+
+// GET new url page
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
+// POST Create a new url
 app.post("/urls", (req, res) => {
   const newLinkID = generateRandomString();
   // Poor handling if the input has a http:// already attached
@@ -44,10 +56,13 @@ app.post("/urls", (req, res) => {
   res.redirect(`/urls/${newLinkID}`);
 });
 
+// GET sends a shortURL to a longURL
 app.get("/u/:shortURL", (req, res) => {
   const longURL = urlDatabase[req.params.shortURL]
   res.redirect(longURL);
 });
+
+
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);

240b18985d2de73b1fee5cd05b7f6f09d853ef42
Noah Vandenberg <noah@noahvandenberg.com>
2022-01-24 17:39:54 -0800

    added random string generator

--- express_server.js
+++ express_server.js
@@ -2,6 +2,16 @@ const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
 
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
+
+
+
+function generateRandomString() {
+  return Math.random().toString(36).slice(7)
+}
+// console.log(generateRandomString())
+
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
@@ -18,13 +28,18 @@ app.get("/urls", (req, res) => {
   res.render('urls_index',templateVars)
 });
 
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
 app.get("/urls/:shortURL", (req, res) => {
   const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
   res.render('urls_show', templateVars)
 });
 
-app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
 });
 
 app.listen(PORT, () => {

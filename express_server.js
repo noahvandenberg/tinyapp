0753066e19cb35e9c328bc6f261fc04e851e9875
Noah Vandenberg <noah@noahvandenberg.com>
2022-01-26 17:21:31 -0800

    linted

--- express_server.js
+++ express_server.js
@@ -5,10 +5,10 @@ const PORT = 8080; // default port 8080
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({extended: true}));
 
-const cookieParser = require('cookie-parser')
-app.use(cookieParser())
+const cookieParser = require('cookie-parser');
+app.use(cookieParser());
 
-function generateRandomString() {
+const generateRandomString = () => {
   return Math.random().toString(36).slice(7);
 };
 
@@ -38,8 +38,8 @@ app.get("/urls", (req, res) => {
     urls: urlDatabase,
     users: users,
     user_id: req.cookies.user_id
-  }
-  res.render('urls_index',templateVars)
+  };
+  res.render('urls_index',templateVars);
 });
 
 // GET registration page
@@ -65,56 +65,62 @@ app.get("/urls/new", (req, res) => {
   const templateVars = {
     users: users,
     user_id: req.cookies.user_id
-  }
+  };
   res.render("urls_new", templateVars);
 });
 
 // GET Specific URL Page
 app.get("/urls/:shortURL", (req, res) => {
-  const templateVars = { 
-    shortURL: req.params.shortURL, 
+  const templateVars = {
+    shortURL: req.params.shortURL,
     longURL: urlDatabase[req.params.shortURL],
     users: users,
     user_id: req.cookies.user_id
   };
-  res.render('urls_show', templateVars)
+  res.render('urls_show', templateVars);
 });
 
 // GET sends a shortURL to a longURL
 app.get("/u/:shortURL", (req, res) => {
-  const longURL = urlDatabase[req.params.shortURL]
+  const longURL = urlDatabase[req.params.shortURL];
   res.redirect(longURL);
 });
 
 
 // Register / Login / Logout
 app.post("/register", (req, res) => {
-  if (req.body.email === '' || req.body.email === '') {res.statusCode = 400; res.redirect('/register')}
-  for (const user in users) {
-    if (req.body.email === users[user].email) {res.statusCode = 400; res.redirect('/register')}
+  if (req.body.email === '' || req.body.email === '') {
+    res.statusCode = 400;
+    res.redirect('/register');
   }
-  const newUserId = generateRandomString() + generateRandomString()
+  for (const user in users) {
+    if (req.body.email === users[user].email) {
+      res.statusCode = 400;
+      res.redirect('/register');
+    }
+  }
+  const newUserId = generateRandomString() + generateRandomString();
   users[newUserId] = {
     id: newUserId,
     email: req.body.email,
     password: req.body.password,
   };
-  res.cookie('user_id', newUserId)
-  res.redirect('/urls')
+  res.cookie('user_id', newUserId);
+  res.redirect('/urls');
 });
 
 app.post("/login", (req, res) => {
   for (const user in users) {
     if (users[user].email === req.body.email && users[user].password === req.body.password) {
-      res.cookie('user_id', users[user].id)
+      res.cookie('user_id', users[user].id);
     }
   }
-  res.redirect('/urls')
+  res.redirect('/urls');
 });
 
 app.post("/logout", (req, res) => {
   res.clearCookie('user_id');
-  res.redirect('/urls')
+  res.redirect('/urls');
 });
 
 
@@ -127,13 +133,13 @@ app.post("/urls", (req, res) => {
 });
 
 app.post("/urls/:shortURL/update", (req, res) => {
-  urlDatabase[req.params.shortURL] = req.body.longURL
-  res.redirect("/urls")
+  urlDatabase[req.params.shortURL] = req.body.longURL;
+  res.redirect("/urls");
 });
 
 app.post("/urls/:shortURL/delete", (req, res) => {
-  delete urlDatabase[req.params.shortURL]
-  res.redirect("/urls")
+  delete urlDatabase[req.params.shortURL];
+  res.redirect("/urls");
 });
 
 

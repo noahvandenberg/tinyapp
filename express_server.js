9bcaf1e00a935173e90baf75bfbc52a110658181
Noah Vandenberg <noah@noahvandenberg.com>
2022-01-26 17:53:40 -0800

    created new urlDatabase format

--- express_server.js
+++ express_server.js
@@ -13,9 +13,16 @@ const generateRandomString = () => {
 };
 
 const urlDatabase = {
-  "b2xVn2": "www.lighthouselabs.ca",
-  "9sm5xK": "www.google.com"
+  b6UTxQ: {
+      longURL: "https://www.tsn.ca",
+      userID: "aJ48lW"
+  },
+  i3BoGr: {
+      longURL: "https://www.google.ca",
+      userID: "aJ48lW"
+  }
 };
+
 
 const users = {
   "admin" : {

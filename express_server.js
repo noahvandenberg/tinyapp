d383989f3dc29434ebbe9949bfe27da595ab56cf
Noah Vandenberg <noah@noahvandenberg.com>
2022-01-27 18:58:09 -0800

    refactored for new error handling

--- express_server.js
+++ express_server.js
@@ -42,28 +42,27 @@ const users = {
 };
 users.admin = userHelpers.createAdminUser(bcrypt);
 
-
-
 app.set('view engine', 'ejs');
 
-app.get("/", (req, res) => {
 
+
+
+
+
+//  **************************** GET ****************************
+app.get("/", (req, res) => {
   if (req.session.user_id) {
     res.redirect("/urls");
   }
-
   if (!req.session.user_id) {
-    console.log(false);
     res.redirect("/login");
   }
-
 });
 
 app.get("/register", (req, res) => {
   if (req.session.user_id) {
     res.redirect('/urls');
   }
-
   if (!req.session.user_id) {
     const templateVars = {
       users: users,
@@ -77,7 +76,6 @@ app.get("/login", (req, res) => {
   if (req.session.user_id) {
     res.redirect('/urls');
   }
-
   if (!req.session.user_id) {
     const templateVars = {
       users: users,
@@ -88,59 +86,44 @@ app.get("/login", (req, res) => {
 });
 
 app.get("/urls", (req, res) => {
-
   if (!req.session.user_id) {
-    //SHOULD REDIRECT TO RELEVENT ERROR PAGE
-    res.redirect('/login');
+    res.redirect('/e/400');
   }
-
   if (req.session.user_id) {
     const usersUrls = userHelpers.getUrlsByUser(req.session.user_id, urlDatabase);
-
     const templateVars = {
       urls: usersUrls,
       users: users,
       user_id: req.session.user_id
     };
-  
     res.render('urls_index',templateVars);
   }
 });
 
 app.get("/urls/new", (req, res) => {
   if (!req.session.user_id) {
-    res.redirect('/login');
+    res.redirect('/e/400');
   }
-
   if (req.session.user_id) {
     const templateVars = {
       users: users,
       user_id: req.session.user_id
     };
-  
     res.render("urls_new", templateVars);
   }
 });
 
 app.get("/urls/:shortURL", (req, res) => {
-
   const shortURL = req.params.shortURL;
-
   if (!req.session.user_id) {
-    // SHOULD REDIRECT TO USER NOT LOGGED IN ERROR PAGE
-    res.redirect('/login');
+    res.redirect('/e/400');
   }
-
   if (!urlDatabase.hasOwnProperty(shortURL)) {
-    // SHOULD REDIRECT TO PAGE NOT FOUND ERROR PAGE
-    res.redirect('/urls');
+    res.redirect('/e/404');
   }
-
   if (urlDatabase[shortURL].userID !== req.session.user_id) {
-    // SHOULD REDIRECT TO FOREBIDDEN / NOT AUTHORIZED ERROR PAGE
-    res.redirect('/urls');
+    res.redirect('/e/403');
   }
-
   if (urlDatabase[shortURL].userID === req.session.user_id && req.session.user_id) {
     const templateVars = {
       shortURL: req.params.shortURL,
@@ -148,23 +131,20 @@ app.get("/urls/:shortURL", (req, res) => {
       users: users,
       user_id: req.session.user_id
     };
-  
     res.render('urls_show', templateVars);
   }
 });
  
 app.get("/u/:shortURL", (req, res) => {
   if (!urlDatabase[req.params.shortURL]) {
-    // SHOULD REDIRECT TO PAGE NOT FOUND ERROR PAGE
-    res.statusCode = 400;
-    res.redirect('/urls');
+    res.redirect('/e/404');
   }
   if (urlDatabase[req.params.shortURL]) {
     const longURL = urlDatabase[req.params.shortURL].longURL;
     res.redirect(longURL);
   }
 });
-///////////////////////////////////////////////////////////////////////////////////////////////////
+
 app.get("/e/:errorCode", (req, res) => {
   const templateVars = {
     users: users,
@@ -174,22 +154,16 @@ app.get("/e/:errorCode", (req, res) => {
   res.render('error', templateVars)
 });
 
+
+//  **************************** POST ****************************
 app.post("/register", (req, res) => {
-
   if (!req.body.email || !req.body.password) {
-    // CANNOT ENTER NO INFO BAD REQUEST
-    res.statusCode = 400;
-    res.redirect('/register');
+    return res.statusCode(400)
   }
-
   const userID = userHelpers.getUserByEmail(req.body.email,users);
-
   if (userID) {
-    // USER ALREADY EXITS SEND ERROR
-    res.statusCode = 400;
-    res.redirect('/login');
+    return res.statusCode(400)
   }
-
   if (!userID) {
     const newUserId = generateRandomString() + generateRandomString();
     users[newUserId] = {
@@ -201,32 +175,21 @@ app.post("/register", (req, res) => {
     req.session.user_id = newUserId;
     res.redirect('/urls');
   }
-
 });
-////////////////////////////////////////////////////////////////////////////////////////////////
+
 app.post("/login", (req, res) => {
-
   if (!req.body.email || !req.body.password) {
-    // CANNOT ENTER NO INFO BAD REQUEST
-    // res.statusCode = 400;
-    res.redirect('/e/400');
+    return res.statusCode(400)
   }
-
   const userID = userHelpers.getUserByEmail(req.body.email,users);
-
   if (!userID) {
-    // USER DOES NOT EXIST
-    res.redirect('/login');
+    return res.statusCode(401)
   }
-
   const userEmail = users[userID].email;
   const userPassword = users[userID].password;
-
   if (!bcrypt.compareSync(req.body.password, userPassword)) {
-    // SHOULD REDIRECT TO BAD PASSWORD IN ERROR PAGE
-    res.redirect('/login');
+    return res.statusCode(401)
   }
-
   if (bcrypt.compareSync(req.body.password, userPassword)) {
     req.session.user_id = userID;
     res.redirect('/urls');
@@ -240,9 +203,7 @@ app.post("/logout", (req, res) => {
 
 app.post("/urls", (req, res) => {
   if (!req.session.user_id) {
-    // SHOULD REDIRECT TO NOT LOGGED / NOT AUTHED IN ERROR PAGE
-    res.statusCode = 400;
-    res.end();
+    return res.statusCode(400)
   }
   if (req.session.user_id) {
     const newLinkID = generateRandomString();
@@ -256,42 +217,37 @@ app.post("/urls", (req, res) => {
 });
 
 app.post("/urls/:shortURL", (req, res) => {
-
   if (!req.session.user_id) {
-    // SHOULD REDIRECT TO NOT LOGGED IN IN ERROR PAGE
-    res.statusCode = 401;
-    res.redirect('/login');
+    return res.statusCode(401)
   }
-  
   if (req.session.user_id !== urlDatabase[req.params.shortURL].userID) {
-    // SHOULD REDIRECT TO NOT AUTHED IN ERROR PAGE
-    res.statusCode = 403;
-    res.redirect('/urls');
+    return res.statusCode(403)
   }
-
   if (req.session.user_id === urlDatabase[req.params.shortURL].userID) {
     urlDatabase[req.params.shortURL].longURL = req.body.longURL;
     res.redirect("/urls");
   }
-
 });
 
 app.post("/urls/:shortURL/delete", (req, res) => {
   if (!req.session.user_id) {
-    // SHOULD REDIRECT TO NOT LOGGED IN IN ERROR PAGE
-    res.statusCode = 401;
-    res.redirect('/login');
+    return res.statusCode(401)
   }
   if (req.session.user_id !== urlDatabase[req.params.shortURL].userID) {
-    // SHOULD REDIRECT TO NOT AUTHED IN ERROR PAGE
-    res.statusCode = 403;
-    res.redirect('/urls');
+    return res.statusCode(403)
   }
   if (req.session.user_id === urlDatabase[req.params.shortURL].userID) {
     delete urlDatabase[req.params.shortURL];
     res.redirect("/urls");
   }
 });
+
+
+
+
+
+
+
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
